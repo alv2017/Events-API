dbc6833be2f262aa35a543101fe9f23580abee8d
alv2017 <v.alishauskaite@gmail.com>
2023-10-14 00:57:09 +0300

    Permissions updated.

--- event/permissions.py
+++ event/permissions.py
@@ -2,5 +2,8 @@ from rest_framework import permissions
 
 
 class IsOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_active
+
     def has_object_permission(self, request, view, obj):
         return bool(obj.arranged_by == request.user)
--- user/permissions.py
+++ user/permissions.py
@@ -3,4 +3,4 @@ from rest_framework import permissions
 
 class IsSuperUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user and request.user.is_superuser
+        return request.user.is_authenticated and request.user.is_active and request.user.is_superuser
