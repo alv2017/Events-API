82a73fc9405545285ede76b9aa29604558051ffa
alv2017 <v.alishauskaite@gmail.com>
2023-10-26 17:14:21 +0300

    Settings updated.

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,8 @@ env = environ.Env(
     ALLOWED_HOSTS=(str, "*"),
     ENABLE_HTTPS=(bool, False),
     DB_URL=(str, f"sqlite:////{os.path.join(BASE_DIR, 'events.sqlite3')}"),
+    LOG_DEFAULT=(str, "/logs/log.log"),
+    LOG_AUTH=(str, "/logs/auth.log")
 )
 
 # Take environment variables from .env file
