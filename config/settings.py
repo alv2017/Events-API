336ca99cc0ea88044ba9a92cdc460925d2ab1863
alv2017 <v.alishauskaite@gmail.com>
2023-11-01 00:28:58 +0200

    code formatting update

--- config/settings.py
+++ config/settings.py
@@ -57,11 +57,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
 ]
 if DEBUG:
-    INSTALLED_APPS += [
-        "debug_toolbar",
-        "drf_spectacular",
-        "drf_spectacular_sidecar"
-    ]
+    INSTALLED_APPS += ["debug_toolbar", "drf_spectacular", "drf_spectacular_sidecar"]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -153,7 +149,7 @@ if DEBUG:
             "django.db.backends": {
                 "handlers": ["console"],
                 "level": "ERROR",
-                "description": "SQL query logger"
+                "description": "SQL query logger",
             }
         }
     )
@@ -274,16 +270,20 @@ SIMPLE_JWT = {
 if DEBUG:
     # Django Debug Toolbar Support
     import socket
+
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + [
+        "127.0.0.1",
+        "10.0.2.2",
+    ]
 
     # DRF Spectacular Support
     SPECTACULAR_SETTINGS = {
-        'TITLE': 'Events API',
-        'DESCRIPTION': 'API that helps to manage user created events',
-        'VERSION': '1.0.0',
-        'SERVE_INCLUDE_SCHEMA': False,
-        'SWAGGER_UI_DIST': 'SIDECAR',  # shorthand to use the sidecar instead
-        'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
-        'REDOC_DIST': 'SIDECAR',
+        "TITLE": "Events API",
+        "DESCRIPTION": "API that helps to manage user created events",
+        "VERSION": "1.0.0",
+        "SERVE_INCLUDE_SCHEMA": False,
+        "SWAGGER_UI_DIST": "SIDECAR",  # shorthand to use the sidecar instead
+        "SWAGGER_UI_FAVICON_HREF": "SIDECAR",
+        "REDOC_DIST": "SIDECAR",
     }
--- config/urls.py
+++ config/urls.py
@@ -22,11 +22,11 @@ if settings.DEBUG:
     urlpatterns += [
         # Django Debug Toolbar
         path("__debug__/", include("debug_toolbar.urls")),
-
         # DRF Spectacular API Schema and Swagger Docs
-        path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
-        path('api/v1/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+        path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+        path(
+            "api/v1/schema/swagger-ui/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
     ]
-
-
-
